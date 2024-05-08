df3172c9af0ffce614f7ea4704bc5434ead0c1aa
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-05-08 11:15:14 +0530

    Create from updated turborepo-template

---
+++ examples/remix/app/routes/_index.tsx
@@ -0,0 +1,11 @@
+/** Remix App */
+export default function Index(): JSX.Element {
+  return (
+    <div className="container">
+      <h1 className="title">
+        Blog <br />
+        <span>Kitchen Sink</span>
+      </h1>
+    </div>
+  );
+}
