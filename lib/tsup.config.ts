3c3f6d822c3672fd13c0686ffd67a11cdcb31c17
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-05-08 13:44:06 +0530

    Simplify build config

--- lib/tsup.config.ts
+++ lib/tsup.config.ts
@@ -3,13 +3,14 @@ import { defineConfig, type Options } from "tsup";
 export default defineConfig(
   (options: Options) =>
     ({
-      format: ["cjs", "esm"],
+      format: ["cjs"],
       target: "es2019",
-      entry: ["./src"],
+      entry: ["./src/index.ts"],
       sourcemap: false,
       clean: !options.watch,
-      bundle: true,
       minify: !options.watch,
+      skipNodeModulesBundle: true,
+      dts: true,
       ...options,
     }) as Options,
 );
