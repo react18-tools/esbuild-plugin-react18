6002d401c8053a49eb73b2fb7c4b3585a58f3f71
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-05-08 13:58:38 +0530

    Fix export issue.

--- lib/tsup.config.ts
+++ lib/tsup.config.ts
@@ -3,14 +3,15 @@ import { defineConfig, type Options } from "tsup";
 export default defineConfig(
   (options: Options) =>
     ({
-      format: ["cjs"],
+      format: ["cjs", "esm"],
       target: "es2019",
       entry: ["./src/index.ts"],
       sourcemap: false,
       clean: !options.watch,
       minify: !options.watch,
-      skipNodeModulesBundle: true,
+      bundle: true,
       dts: true,
+      footer: { js: "module.exports = module.exports.default;" },
       ...options,
     }) as Options,
 );
